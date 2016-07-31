3f1c78b42e19297dfd9472d3c4425df72c6cb3a6
Leonid Borisevich <leonidborisevich00@gmail.com>
2016-07-31 13:58:35 +0300

    no message

--- src/AtomicSubmarine.java
+++ src/AtomicSubmarine.java
@@ -1,5 +1,33 @@
+import java.lang.annotation.*;
+
 /**
  * Created by Leonid on 28.07.2016.
  */
 public class AtomicSubmarine {
+    class Engine{
+        public void dive(){
+            System.out.println("Diving...");
+        }
+    }
+
+    @FielAnnotation(info = "This is the engine")
+    private Engine engine;
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public static void main(String[] args){
+        AtomicSubmarine submarine = new AtomicSubmarine();
+        submarine.engine = submarine.new Engine();
+        submarine.getEngine().dive();
+    }
+}
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@interface FielAnnotation{
+    String info() default "";
 }
--- src/StudentGroups.java
+++ src/StudentGroups.java
@@ -1,5 +1,65 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Leonid on 28.07.2016.
+ *
+ * TODO: архитектура???
  */
 public class StudentGroups {
+    public static void main(String[] args){
+        Map<Student, Subjects[]> map = new HashMap<Student, Subjects[]>();
+        map.put(new Student("Ivan", "Ivanov"), new Subjects[]{Subjects.MATHS, Subjects.ENGLISH});
+        map.get(new Student("Ivan", "Ivanov"));
+        Subjects kek = Subjects.ENGLISH;
+        //kek.mark = 1.0;
+    }
+}
+
+enum Subjects{
+    MATHS{
+        int mark;
+    },
+    ENGLISH{
+        float mark;
+    },
+    HISTORY{
+        float mark;
+    },
+    PE{
+        int mark;
+    };
+}
+
+class Student{
+    private String firstName;
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    private String lastName;
+
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
 }
